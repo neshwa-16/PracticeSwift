9a93c72c6bce685dd8b67ab2ad2a5f74ae0cef72
neshwa parveen <neshwaparveenn@gmail.com>
2025-04-29 23:19:35 +0530

    animation timings

---
+++ PracticeSwift/Views/AnimationTiming.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationTiming.swift
+//  PracticeSwift
+//
+//  Created by Hayal on 29/04/25.
+//
+
+import SwiftUI
+
+struct AnimationTiming: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationTiming()
+}
