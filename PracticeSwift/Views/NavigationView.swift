98aa723c17b30c4bd7634812adaca5124d4baa68
neshwa parveen <neshwaparveenn@gmail.com>
2025-05-11 02:08:52 +0530

    navigation view - complete

--- PracticeSwift/Views/NavigationView.swift
+++ PracticeSwift/Views/NavigationView.swift
@@ -13,11 +13,27 @@ struct NavigationViews: View {
         NavigationView {
             ScrollView {
                 NavigationLink("hello world!!!", destination: {
-                    Text("Second Screen")
+                    ScondScreen()
                 })
                 Text("Hello, World!")
             }
             .navigationTitle("All Inboxes")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading, content: {
+                    HStack(content: {
+                        Image(systemName: "person.fill")
+                        Image(systemName: "flame.fill")
+                    })
+                })
+
+                ToolbarItem(placement: .topBarTrailing, content: {
+                    NavigationLink(destination: ScondScreen(), label: {
+                        Image(systemName: "gear")
+                    })
+                    .tint(.red)
+                })
+                
+            }
             //.navigationBarTitleDisplayMode(.automatic)
             //.navigationBarTitleDisplayMode(.inline)
             //.toolbar(.hidden)
@@ -25,6 +41,30 @@ struct NavigationViews: View {
     }
 }
 
+struct ScondScreen: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+                .navigationTitle("Green Screen")
+                .toolbar(.hidden)
+            
+            VStack {
+                Button("Back Button", action: {
+                    dismiss()
+                })
+                NavigationLink("Click here", destination: {
+                    Text("Third Screen")
+                })
+            }
+            
+            
+        }
+    }
+}
+
 #Preview {
     NavigationViews()
 }
+
